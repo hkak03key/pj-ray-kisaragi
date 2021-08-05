57ebfa1974bda2db1e5ba72ac808a156b1e0b929
Hikaru <hkak03key@gmail.com>
2021-08-05 06:17:51 +0000

    append google project services

--- terraform/envs/gcp_prod/google_project_service.tf
+++ terraform/envs/gcp_prod/google_project_service.tf
@@ -1,5 +1,11 @@
 resource "google_project_service" "services" {
   for_each = toset([
+    "calendar-json.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    "cloudscheduler.googleapis.com",
+    "iam.googleapis.com",
+    "secretmanager.googleapis.com",
+    "sheets.googleapis.com",
   ])
 
   project                    = local.project_id
